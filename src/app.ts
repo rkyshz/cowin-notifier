69311c8b6a7cac2c14660fe30fb12bdb75731e02
rakz <rkyshnan@gmail.com>
2021-06-14 16:06:26 +0530

    notification

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { CowinService } from './CowinService';
 import { CowinResponse } from './CowinResponse';
 import _ from 'lodash';
 //import * as config from '../cowin-config.json';
+import audic from 'audic';
 
 
 
@@ -25,15 +26,20 @@ function getAllResults() {
 }
 
 function processData(data:CowinResponse[]){
+    let finalResults:Array<string>=[];
     data.forEach((center)=>{
         center.sessions
         .filter(s=>s.available_capacity > 1)
-        .forEach(slot=>console.log(`Age ${slot.min_age_limit} - Dose-1 : ${slot.available_capacity_dose1} & Dose-2 : ${slot.available_capacity_dose2} @ ${center.name} (${center.pincode})`));
+        .forEach(slot=>finalResults.push(`Age ${slot.min_age_limit} - Dose-1 : ${slot.available_capacity_dose1} & Dose-2 : ${slot.available_capacity_dose2} @ ${center.name} (${center.pincode})`));
     });
     
+    if(finalResults.length>1){
+        finalResults.forEach(res=>console.log(res));
+        new audic("notify.mp3").play();
+    }
 }
 
-getAllResults();
-//setInterval(()=>getAllResults(),2000);
+//getAllResults();
+setInterval(()=>getAllResults(),10000);
 
 
