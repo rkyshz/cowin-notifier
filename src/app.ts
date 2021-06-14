4f660a5efd349067cdcd06d379be1fd57d08c489
rakz <rkyshnan@gmail.com>
2021-06-14 19:31:50 +0530

    time param

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { CowinService } from './CowinService'; 
+import { CowinService } from './CowinService';
 import { CowinResponse } from './CowinResponse';
 import _ from 'lodash';
 //import * as config from '../cowin-config.json';
@@ -12,14 +12,14 @@ let request: {
     dateFrom: string,
     dateTo: string,
     type: string
-}[]=JSON.parse(require('fs').readFileSync('cowin-config.json', 'utf8'));
+}[] = JSON.parse(require('fs').readFileSync('cowin-config.json', 'utf8'));
 
 
 function getAllResults() {
     let requests: Array<Promise<CowinResponse[]>> = [];
-    request.forEach(async (val, key) =>{
+    request.forEach(async val => {
         for (let d = new Date(val.dateFrom); d <= new Date(val.dateTo); d.setDate(d.getDate() + 1)) {
-            val.type==='P' ? requests.push(new CowinService().getResultByPincode(val.param, toDDMMYYYY(d))) : requests.push(new CowinService().getResultByDistrict(val.param, toDDMMYYYY(d)))
+            val.type === 'P' ? requests.push(new CowinService().getResultByPincode(val.param, toDDMMYYYY(d))) : requests.push(new CowinService().getResultByDistrict(val.param, toDDMMYYYY(d)))
         }
 
     }
@@ -30,24 +30,27 @@ function getAllResults() {
         processData(result);
     })
 }
-function toDDMMYYYY(date:Date):string{
-    return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+function toDDMMYYYY(date: Date): string {
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 }
-function processData(data:CowinResponse[]){
-    let finalResults:Array<string>=[];
-    data.forEach((center)=>{
+function processData(data: CowinResponse[]) {
+    let finalResults: Array<string> = [];
+    data.forEach((center) => {
         center.sessions
-        .filter(s=>s.available_capacity > 1)
-        .forEach(slot=>finalResults.push(`Age ${slot.min_age_limit} - Dose-1 : ${slot.available_capacity_dose1} & Dose-2 : ${slot.available_capacity_dose2} @ ${center.name} (${center.pincode})`));
+            .filter(s => s.available_capacity > 1)
+            .forEach(slot => finalResults.push(`Age ${slot.min_age_limit} - Dose-1 : ${slot.available_capacity_dose1} & Dose-2 : ${slot.available_capacity_dose2} @ ${center.name} (${center.pincode})`));
     });
 
-    if(finalResults.length>1){
-        finalResults.forEach(res=>console.log(`[${new Date().toISOString()}] ${res}`));
+    if (finalResults.length > 1) {
+        finalResults.forEach(res => console.log(`[${new Date().toISOString()}] ${res}`));
         new audic("notify.mp3").play();
     }
 }
 
+let argv = require('minimist')(process.argv.slice(2));
+let interval=argv.t || 5;
+console.log(`Polling every ${interval} minutes`);
 //getAllResults();
-setInterval(()=>getAllResults(),60000);
+setInterval(() => getAllResults(), interval*60000);
 
 
