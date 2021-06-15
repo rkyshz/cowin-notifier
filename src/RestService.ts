51dfbdf6ea408c5d6c006127bbaf0077d77c1e92
rkyshz <mail@rakz.in>
2021-06-15 21:31:31 +0530

    uaer agent and content type

--- src/RestService.ts
+++ src/RestService.ts
@@ -4,7 +4,13 @@ import { CowinResponse } from './CowinResponse';
 export class RestService {
 
     static doGet(url: string): Promise<CowinResponse[]> {
-        return fetch(url)
+        return fetch(url,{
+            method: 'get',
+            headers: { 
+                'Content-Type': 'application/json',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' 
+            },
+        })
             .then(res => res.json())
             .then(res => {
                 return res['centers'] as CowinResponse[];
